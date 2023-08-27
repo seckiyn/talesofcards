bc2cdb6012f04888e48dcb03dd63fc50544edfd1
Mustafa Akkaya <kyofl6@gmail.com>
2023-08-27 15:41:22 +0300

    Linting

--- pggui.py
+++ pggui.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""
+    Pygame UI for the game
+"""
 import pygame
 from random import choice as c
 from typing import Tuple, Iterable
@@ -7,10 +11,6 @@ from gameobjects import Card, GameContainer, HealthSprite, ShieldSprite
 pygame.init()
 pygame.font.init()
 
-"""
-self.width = 1500
-self.height = 750
-"""
 
 # Screen Setup
 WIDTH: int = 1500
@@ -21,23 +21,32 @@ FPS = 60
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, imagepath: str):
+        """
+            Base Sprite class
+        """
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(imagepath)
         self.rect = self.image.get_rect()
 
 
 def draw_line(surface, cord1, cord2, width=3, color="purple"):
+    """
+        Draw lines for debug
+    """
     pygame.draw.line(surface, color, cord1, cord2, width)
 
 
 def draw_guide_lines(surface):
-    HALFWIDTH:int  = WIDTH // 2
-    THIRDHEIGHT:int = HEIGHT // 3
+    """
+        Draw guide lines for geometry
+    """
+    HALFWIDTH: int = WIDTH // 2
+    THIRDHEIGHT: int = HEIGHT // 3
     # Board-Top Horizontal line
     cord1_x = 0
     cord1_y = THIRDHEIGHT
     cord2_x = WIDTH
-    cord2_y = cord1_y 
+    cord2_y = cord1_y
     cord1 = cord1_x, cord1_y
     cord2 = cord2_x, cord2_y
     draw_line(surface, cord1, cord2)
@@ -135,7 +144,7 @@ def draw_guide_lines(surface):
 
 def spread_card(card_count: int, size: float, cardsize=157, padding=10) -> Tuple[int, ...]:
     """
-    -----------------------
+        Returns a tuple of x values to spread cards in hand
     """
     halfsize = size // 2
     halfpadding = padding // 2
@@ -147,7 +156,7 @@ def spread_card(card_count: int, size: float, cardsize=157, padding=10) -> Tuple
     if card_count > 8:
         card_count = 8
     if card_count == 1:
-        return size // 2,
+        return (size // 2,)
     if card_count == 2:
         left = halfsize - halfpadding
         right = halfsize + halfpadding
@@ -168,40 +177,19 @@ def spread_card(card_count: int, size: float, cardsize=157, padding=10) -> Tuple
     mostright = right + padding + cardsize
     return mostleft, left, *middles, right, mostright
 
-"""
-    if card_count == 4:
-        left, right = spread_card(2, size)
-        mostleft = left - padding - cardsize
-        mostright = right + padding + cardsize
-        return mostleft, left, right, mostright
-    if card_count == 5:
-        left, middle, right = spread_card(3, size)
-        mostleft = left - padding - cardsize
-        mostright = right + padding + cardsize
-        return mostleft, left, middle, right, mostright
-    if card_count == 6:
-        left, *middle, right = spread_card(4, size)
-        mostleft = left - padding - cardsize
-        mostright = right + padding + cardsize
-        return (mostleft, left, *middle, right, mostright)
-    if card_count == 7:
-        left, *middle, right = spread_card(5, size)
-        mostleft = left - padding - cardsize
-        mostright = right + padding + cardsize
-        return (mostleft, left, *middle, right, mostright)
-    if card_count == 8:
-        left, *middle, right = spread_card(5, size)
-        mostleft = left - padding - cardsize
-        mostright = right + padding + cardsize
-        return (mostleft, left, *middle, right, mostright)
-"""
 
 def draw_circles(surface, x_cords: Tuple[int, ...]) -> None:
+    """
+        Drawing circle wrapper to be removed
+    """
     for x_cord in x_cords:
         pygame.draw.circle(surface, "red", (x_cord, 5 * (HEIGHT // 6)), 157)
 
 
 def draw_rects(surface, x_cords: Tuple[int, ...]) -> None:
+    """
+        Drawing rectangles wrapper to be removed
+    """
     for x_cord in x_cords:
         width = 157
         height = 250
@@ -211,6 +199,10 @@ def draw_rects(surface, x_cords: Tuple[int, ...]) -> None:
 
 
 def adjust_surfaces(surface: pygame.Surface, surf: Iterable[Card]) -> None:
+    """
+        Takes the tuple of surfaces(surf) and adjusts them according to
+        x values given by spread_card function
+    """
     if not surf:
         return
     x_cords = spread_card(len(surf), WIDTH)
@@ -218,18 +210,23 @@ def adjust_surfaces(surface: pygame.Surface, surf: Iterable[Card]) -> None:
         card.rect.center = x_cord, 5 * (HEIGHT // 6)
 
 
-# Pygame
+# Pygame #TODO: Add these to the GAME_CONTAINER class to easily
 screen = pygame.display.set_mode(WINDOW_SIZE)
 clock = pygame.time.Clock()
 delta_time = 0
 sprite_group = pygame.sprite.Group()
 board_sprite_group = pygame.sprite.Group()
 gui_sprite_group = pygame.sprite.Group()
+dead_sprite_group = pygame.sprite.Group()
 game_container = GameContainer(WIDTH, HEIGHT)
 
 
+
 def click_event_player(card: Card):
-    # TODO: add a last card list
+    """
+        What to do when player clicks a card sprite
+    """
+    dead_sprite_group.add(*board_sprite_group)
     board_sprite_group.empty()
     card.kill()
     card.move_to(WIDTH//2, HEIGHT//2)
@@ -240,8 +237,12 @@ def create_gui() -> Tuple[Sprite, Sprite]:
     """
         Will return health and shield sprites
     """
-    ...
+    return 34 + 35
+
 def Game() -> None:
+    """
+        Start the game
+    """
     running = True
     card = Card("card", "./cards/card.png")
     card2 = Card("card2", "./cards/card2.png")
@@ -264,7 +265,7 @@ def Game() -> None:
                 if event.key == pygame.K_k:
                     if card_count < 8:
                         cardname = c(['bomb', 'card', 'card2'])
-                        imagepath =  f"./cards/{cardname}.png"
+                        imagepath = f"./cards/{cardname}.png"
                         tmp_card = Card("bomb", imagepath)
                         sprite_group.add(tmp_card)
                     card_count += 1
